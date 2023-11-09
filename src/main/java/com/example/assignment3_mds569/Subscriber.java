682050a44861b30ad791958fdc1730dff0f33d1d
Matthew S <mds569@usask.ca>
2023-11-09 01:23:00 -0600

    Removed old modelChanged() from Subscriber interface

--- src/main/java/com/example/assignment3_mds569/Subscriber.java
+++ src/main/java/com/example/assignment3_mds569/Subscriber.java
@@ -4,10 +4,6 @@ import java.util.List;
 
 public interface Subscriber {
 
-    //void modelChanged();
-
-    //void iModelChanged();
-
     // The method any Subscriber must implement
     void getNotification(String chnl);
 
